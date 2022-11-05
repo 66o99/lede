583cf844f579f2fe2b5a71b084de413a7cbdadd2
66o99 <116540988+66o99@users.noreply.github.com>
2022-11-05 14:08:18 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -44,7 +44,7 @@ DEFAULT_PACKAGES.router:=\
 	dnsmasq-full firewall iptables ppp ppp-mod-pppoe \
 	coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
 	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi \
-	ddns-scripts_aliyun ddns-scripts_dnspod curl ca-certificates
+	ddns-scripts_aliyun ddns-scripts_dnspod curl ca-certificates luci-app-arpbind
         
 	
 #	dnsmasq-full firewall iptables ppp ppp-mod-pppoe \
